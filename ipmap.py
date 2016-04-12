05f0af75c42d54ac356db52b467d6353e87f0dfa
alexcomu <alex.comunian@gmail.com>
2016-04-12 12:15:37 +0200

    some improvements

--- ipmap.py
+++ ipmap.py
@@ -1,27 +1,27 @@
-
-
-import csv
-
+import csv, sys
 from pymongo import MongoClient
-connection = MongoClient()
+
+def process(inputfile):
+    connection = MongoClient()
+
+    db = connection.get_database('geoip')
+    collection = db.get_collection('geoip')
+    with open(inputfile, 'rb') as csvfile:
+        ipreader = csv.reader(csvfile, delimiter=',')
+        for row in ipreader:
+            data = collection.insert_one(
+                {
+                    "range1":int(row[0]),
+                    "range2":int(row[1]),
+                    "country_code":row[2],
+                    "country":row[3]
+                })
+            print "Insert: ", data
 
 
-db = connection.get_database('ip2location')
-collection = db.get_collection('ipmap')
-
-
-with open('0_ip2location_split.csv', 'rb') as csvfile:
-    ipreader = csv.reader(csvfile, delimiter=',')
-    for row in ipreader:
-        cacca = collection.insert_one(
-            {
-                "range1":row[0],
-                "range2":row[1],
-                "county_code":row[2],
-                "country":row[3]
-            })
-
-        print cacca
-
-        
-
+if __name__ == '__main__':
+    if len(sys.argv) != 2:
+        print "ERROR! Correct usage is:\n"
+        print "\t$ python SCRIPT_NAME INPUT_FILE\n"
+        sys.exit()
+    process(sys.argv[1])
